2091c7a9cd67fc433fe69dda8ccabf71d3591c7a
Michaël Schuh <michael.schuh.34@gmail.com>
2012-09-08 22:30:17 +0200

    intégration mongodb odm

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new Schuh\BlogBundle\SchuhBlogBundle(),
+            new Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
