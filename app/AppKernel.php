193b3cb3e6d198550bb6825becd49a2f9d7ff500
Michaël Schuh <michael.schuh.34@gmail.com>
2012-09-08 22:57:32 +0200

    install/config FOSUserBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,6 +21,7 @@ class AppKernel extends Kernel
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new Schuh\BlogBundle\SchuhBlogBundle(),
             new Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
